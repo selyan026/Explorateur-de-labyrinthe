3009b0b0e7c5b705fdef6e5607a84e2f4035dc28
selyan026 <selyan.idri@gmail.com>
2025-04-22 17:49:36 +0200

    Création du git et packatage

---
+++ src/dijkstra/Dijkstra.java
@@ -0,0 +1,15 @@
+package dijkstra;
+
+import graph.Graph;
+import graph.ShortestPath;
+
+import java.util.HashMap;
+
+public class Dijkstra<T> implements ShortestPath<T> {
+    @Override
+    public Distances<T> compute(Graph<T> g, T src, Animator<T> animator) {
+        ShortestPath.Distances<T> r = new Distances<>(new HashMap<>(), new HashMap<>());
+        return null;
+    }
+
+}
