5856995b1fe392912622063797f3ae53e2152c66
selyan026 <selyan.idri@gmail.com>
2025-05-11 15:03:30 +0200

    Implémentation de la méthode compute dans la class Djikstra, les test de DjikstraTest sont valident

--- src/dijkstra/Dijkstra.java
+++ src/dijkstra/Dijkstra.java
@@ -1,14 +1,40 @@
 package dijkstra;
 
 import graph.Graph;
-
-import java.util.HashMap;
+import java.util.*;
 
 public class Dijkstra<T> implements ShortestPath<T> {
+
     @Override
     public Distances<T> compute(Graph<T> g, T src, Animator<T> animator) {
-        ShortestPath.Distances<T> r = new Distances<>(new HashMap<>(), new HashMap<>());
-        return null;
-    }
+        Map<T, Integer> dist = new HashMap<>();
+        Map<T, T> pred = new HashMap<>(); // Stock les prédécesseurs
+        Set<T> visited = new HashSet<>(); // Sommets déjà visités
+        PriorityQueue<T> file = new PriorityQueue<>(Comparator.comparingInt(dist::get));
 
+        // Initialisation avec un distance de 0 et pas de prédécesseurs
+        dist.put(src, 0);
+        pred.put(src, null);
+        file.add(src);
+
+        // parcours les sommets
+        while (!file.isEmpty()) {
+            T sommet = file.poll();
+            if (!visited.add(sommet)) continue;
+            animator.accept(sommet, dist.get(sommet));
+
+            // exploration des voisins
+            for (Graph.Arc<T> arc : g.getSucc(sommet)) {
+                if (arc.val() < 0) throw new IllegalArgumentException();
+                int newDist = dist.get(sommet) + arc.val();
+
+                if (dist.getOrDefault(arc.dst(), Integer.MAX_VALUE) > newDist) {
+                    dist.put(arc.dst(), newDist);
+                    pred.put(arc.dst(), sommet);
+                    file.add(arc.dst());
+                }
+            }
+        }
+        return new Distances<>(dist, pred);
+    }
 }
