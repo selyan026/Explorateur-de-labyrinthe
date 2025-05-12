45545c8dc6333414fc62e0ecc6e950b9d828a829
Ali Ben Akremi <ali.ben-akremi@etu.u-paris.fr>
2025-05-12 22:20:00 +0200

    Code de la classe GraphMaze et test (+MAJ du jdk (trop récent))

--- src/adaptator/GraphMaze.java
+++ src/adaptator/GraphMaze.java
@@ -1,4 +1,25 @@
 package adaptator;
 
-public class GraphMaze {
+import graph.Graph;
+import maze.Maze;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+
+public class GraphMaze <C> implements Graph <C> {
+    private final Maze<C> maze;
+
+    public GraphMaze(Maze<C> maze) {
+        this.maze = maze;
+    }
+    @Override
+    public List<Arc<C>> getSucc(C cell) {
+        List<Arc<C>> successeurs = new ArrayList<>();
+        Set<C> voisins = maze.openedNeighbours(cell);
+        for (C voisin : voisins) {
+            successeurs.add(new Arc<>(1, voisin));
+        }
+        return successeurs;
+    }
 }
